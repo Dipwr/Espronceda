a33fbd9c13e49d1776bb5191d311ee281602e639
Dipwr <deopwr@gmail.com>
2025-03-01 04:03:25 +0100

    Starting Scene Design

---
+++ scripts/camera.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@export var player : CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !(player.position.x <= -105) && !(player.position.x >= 329):
+		position.x = player.position.x
---
+++ scripts/initial_animation.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func _ready() -> void:
+	pass
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/Calle.tscn")
